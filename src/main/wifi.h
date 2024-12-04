df1662cecec03bd16bb3f7f3b06c511a37ab3872
author <kaushik.ghose@gmail.com>
2024-12-03 21:43:50 -0500

    Add header comment.

--- src/main/wifi.h
+++ src/main/wifi.h
@@ -1,3 +1,15 @@
+/*
+ * Manages WiFi connection for the program.
+ * Part of the Antifreeze program. https://github.com/kghose/antifreeze
+ *
+ * Adapted from
+ * https://github.com/espressif/esp-idf/blob/master/examples/wifi/getting_started/station/main/station_example_main.c
+
+ * (c) 2024 Kaushik Ghose
+ *
+ * Released under the MIT License
+ */
+
 #ifndef _WIFI_H_
 #define _WIFI_H_
 
